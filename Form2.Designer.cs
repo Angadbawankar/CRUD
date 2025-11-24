208cbe45f191af0337ed60bdbac582e6571ba9ee
Angadbawankar <108011679+Angadbawankar@users.noreply.github.com>
2025-11-24 22:43:45 +0530

    Add files via upload

---
+++ Form2.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 16F);
+            label1.Location = new Point(272, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(222, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Employee Details";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F);
+            label2.Location = new Point(207, 74);
+            label2.Name = "label2";
+            label2.Size = new Size(112, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Emp No :";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 14F);
+            label3.Location = new Point(175, 143);
+            label3.Name = "label3";
+            label3.Size = new Size(144, 32);
+            label3.TabIndex = 2;
+            label3.Text = "Emp Name :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 14F);
+            label4.Location = new Point(256, 212);
+            label4.Name = "label4";
+            label4.Size = new Size(63, 32);
+            label4.TabIndex = 3;
+            label4.Text = "Job :";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 14F);
+            label5.Location = new Point(230, 281);
+            label5.Name = "label5";
+            label5.Size = new Size(89, 32);
+            label5.TabIndex = 4;
+            label5.Text = "Salary :";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 14F);
+            textBox1.Location = new Point(349, 71);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(226, 39);
+            textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 14F);
+            textBox2.Location = new Point(349, 140);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(226, 39);
+            textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Font = new Font("Segoe UI", 14F);
+            textBox3.Location = new Point(349, 209);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(226, 39);
+            textBox3.TabIndex = 7;
+            // 
+            // textBox4
+            // 
+            textBox4.Font = new Font("Segoe UI", 14F);
+            textBox4.Location = new Point(349, 278);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(226, 39);
+            textBox4.TabIndex = 8;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(109, 347);
+            button1.Name = "button1";
+            button1.Size = new Size(142, 55);
+            button1.TabIndex = 9;
+            button1.Text = "NEXT";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(337, 347);
+            button2.Name = "button2";
+            button2.Size = new Size(142, 55);
+            button2.TabIndex = 10;
+            button2.Text = "NEW";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.AccessibleRole = AccessibleRole.Application;
+            button3.Location = new Point(565, 347);
+            button3.Name = "button3";
+            button3.Size = new Size(142, 55);
+            button3.TabIndex = 11;
+            button3.Text = "INSERT";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(109, 436);
+            button4.Name = "button4";
+            button4.Size = new Size(142, 55);
+            button4.TabIndex = 12;
+            button4.Text = "UPDATE";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(337, 436);
+            button5.Name = "button5";
+            button5.Size = new Size(142, 55);
+            button5.TabIndex = 13;
+            button5.Text = "DELETE";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(565, 436);
+            button6.Name = "button6";
+            button6.Size = new Size(142, 55);
+            button6.TabIndex = 14;
+            button6.Text = "CLOSE";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(800, 517);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            ForeColor = SystemColors.ActiveCaptionText;
+            Name = "Form2";
+            Text = "Form2";
+            Load += Form2_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace WinFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection("Data Source = AngadLegion\\MSSQLSERVER01; DataBase = CSDB; Integrated Security=SSPI; TrustServerCertificate = True");
+            cmd = new SqlCommand();
+            cmd.Connection = con;
+            con.Open();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            cmd.CommandText = "SELECT * FROM Emp";
+            dr = cmd.ExecuteReader();
+            ShowData();
+        }
+        private void ShowData()
+        {
+            if (dr.Read())
+            {
+                textBox1.Text = dr[0].ToString();
+                textBox2.Text = dr[1].ToString();
+                textBox3.Text = dr[2].ToString();
+                textBox4.Text = dr[5].ToString();
+            }
+            else
+            {
+                MessageBox.Show("End of Records");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ShowData();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is TextBox)
+                {
+                    TextBox tb = ctrl as TextBox;
+                    tb.Clear();
+                }
+            }
+            dr.Close();
+            cmd.CommandText = "Select IsNull(Max(Empno), 1000) + 1 From Emp";
+            textBox1.Text = cmd.ExecuteScalar().ToString();
+            textBox2.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            cmd.CommandText = "Insert Into Emp (Empno,Ename,Job, Salary) Values(" + textBox1.Text + ", '" + textBox2.Text + "', '" + textBox3.Text + "', '" + textBox4.Text + "')";
+            if (cmd.ExecuteNonQuery() > 0)
+            {
+                MessageBox.Show("Insert operations is successful.", "Success", MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+                LoadData();
+            }
+            else
+            {
+                MessageBox.Show("Failed inserting record into the table.", "Failure", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Update operation is successful.", "Success", MessageBoxButtons.OK);
+            cmd.CommandText = "Delete From Emp Where Empno = " + textBox1.Text;
+        }
+    }
+}
